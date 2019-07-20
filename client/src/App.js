448ccead888ad9717557ea4d5506a4341509d33f
Melissa Sheehan <mxsheehan7@gmail.com>
2019-07-20 15:24:46 -0700

    initial setup

---
+++ client/src/App.js
@@ -0,0 +1,57 @@
+// client/src/App.js
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            YAY HI ANN
+          </p>
+          {/*
+           <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        */}
+        </header>
+      </div>
+    );
+    // return <div>I'M READY TO USE THE BACK END APIS! :-)</div>;
+  }
+}
+          // <img src={logo} className="App-logo" alt="logo" />
+
+export default App;
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+// }
+
+// export default App;
